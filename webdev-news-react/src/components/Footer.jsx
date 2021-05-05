9ee470b5d0de30a28a81f8fc3175ca9a35a9497e
j-scu486 <nocturne486@gmail.com>
2021-05-05 12:30:21 +0900

    add: footer links

--- webdev-news-react/src/components/Footer.jsx
+++ webdev-news-react/src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__links"></div>
+            <div className="footer__links">
+                <p>Created by Joseph Scutella</p>
+                <a className="footer__link" href="https://www.josephscutella.com" target="blank" rel="noopener">
+                    Portfolio <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon>
+                </a>
+            </div>
         </footer>
     )
 }
