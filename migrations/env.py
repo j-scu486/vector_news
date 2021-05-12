fc9b097b264ae4a1538452fcaaa7039f4c927521
j-scu486 <nocturne486@gmail.com>
2021-05-12 23:28:56 +0900

    redis

--- migrations/env.py
+++ migrations/env.py
@@ -86,7 +86,7 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
-
+        
         with context.begin_transaction():
             context.run_migrations()
 
